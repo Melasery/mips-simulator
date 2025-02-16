04e0e5932e2f1063cab84a475d910ddd7edf0c0e
Melasery <melasery@wesleyan.edu>
2025-02-16 13:37:36 -0500

    Create global.h

---
+++ src/global.h
@@ -0,0 +1,12 @@
+#ifndef GLOBAL_H_					/* Include guard */
+#define GLOBAL_H_
+
+#include <stdint.h>
+
+extern uint32_t	pc;					/* The PC */
+
+extern uint32_t registers[32];
+extern uint32_t HI;
+extern uint32_t LO;
+
+#endif // GLOBAL_H_
